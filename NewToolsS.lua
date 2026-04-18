c2d62ebadbbabefad82d8af10ddfccc035445fee
dockgoredfinger1-collab <dockgoredfinger1@gmail.com>
2026-04-19 05:37:07 +0800

    Update NewToolsS.lua

--- NewToolsS.lua
+++ NewToolsS.lua
@@ -17,7 +17,7 @@ end
 
 local isOpen = false
 local activeTab = "Models"
-local PROXY_URL = "https://roblox-three-mauve.vercel.app/api/search"
+local PROXY_URL = "https://roblox-three-mauve.vercel.app/api/test"
 
 -- ═══════════════════════════════════
 --           SCREEN GUI
